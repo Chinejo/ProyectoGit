0c3b3249f1f941174f2f37f494409fce89eda2e0
Ale <197334889+Chinejo@users.noreply.github.com>
2025-09-16 16:53:32 -0300

    realizo el footer con las redes sociales

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./Components/Footer"
 import Header from "./Components/Header"
 
 
@@ -8,6 +9,7 @@ function App() {
    <>
     <Header />
    
+   <Footer /> 
    </>
   )
 }
---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+            <div><p>Bienvenido a mi footer</p></div>
+
+            <div className="redes">Instagram</div>
+            <div className="redes">Facebook</div>
+            <div className="redes">LinkedIn</div>
+            <div className="redes">GitHub</div>
+            <div className="redes">www.mipaginaweb.com</div>
+    </div>
+  )
+}
+
+export default Footer
+        
