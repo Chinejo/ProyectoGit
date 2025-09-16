3a5f52a0dc40e6ea9a09e1e0a04e7b1b35f5966d
Ale <197334889+Chinejo@users.noreply.github.com>
2025-09-16 16:40:49 -0300

    realizo el header con sus hipervinculos

---
+++ frontend/src/Components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+       <div>
+        <img src="LOGO" alt="" />
+       </div>
+
+       <div><p>Hola este es el header</p></div>
+       <div>
+        <a href=""> <p>Inicio</p></a>
+        <a href=""> <p>Quienes somos</p></a>
+        <a href=""> <p>Contacto</p></a>
+        <a href=""> <p>Servicios</p></a>
+       </div>
+       <div><h4>Inicio de Sesion</h4></div>
+    </div>
+  )
+}
+
+export default Header
